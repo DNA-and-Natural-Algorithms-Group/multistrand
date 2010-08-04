120789179a4a555a406f24cba4ea00ec5d095f92
Joseph Schaeffer <schaeffer@dna.caltech.edu>
2010-08-04 06:25:25 -0700

    Incremental update for python_options.py.

---
+++ python_options.py
@@ -0,0 +1,21 @@
+################################################################################
+#                                                                              #
+# Python implementation of the options object.                                 #
+# Copyright blah blah blah 2010 Caltech                                        #
+# Written by:  Joseph Schaeffer.                                               #
+#                                                                              #
+# (others add your names as you modify files)                                  #
+#                                                                              #
+################################################################################
+
+class MultistrandOptions( object ):
+    def __init__(self):
+        """ Creates an options object with default (presumably useful) values """
+
+
+        ####################
+        #
+        # BEGIN energymodel
+        #
+        ####################
+        
