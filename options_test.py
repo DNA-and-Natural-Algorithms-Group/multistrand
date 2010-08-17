22589a22019dabeeb23a9a06fbbe61af1f24bac3
Chris Berlind" <cberlind@dna.caltech.edu>
2010-08-17 14:59:56 -0700

    Added a stopcomplexes propert to test object.

--- options_test.py
+++ options_test.py
@@ -1,4 +1,4 @@
-
+from python_objects import *
 
 class Options(object):
   
@@ -8,6 +8,14 @@ class Options(object):
     self._integer = 6
     self.list_of_strings = ["ACT", "CAT", "GAC", "TAG"]
     
+    strand1 = Strand("S1", "S1", "GATTACA", [Domain("d1", "d1", 7)])
+    strand2 = Strand("S2", "S2", "GATTACC", [Domain("d2", "d2", 7)])
+    complex1 = Complex("C1", "C1", [strand1, strand2], [])
+    complex2 = Complex("C2", "C2", [strand1], [])
+    complex3 = Complex("C3", "C3", [strand2], [])
+    self.stopcomplexes = [StopCondition("TAG1", [(complex1, 4, 5), (complex2, 4, 5)]),
+                          StopCondition("TAG2", [(complex2, 6, 0), (complex3, 6, 1)])]
+  
   @property
   def integer(self):
     return self._integer
@@ -17,5 +25,4 @@ class Options(object):
   
   def one_arg_no_return(self, number):
     print "You gave me %.1f" % number
-
 
