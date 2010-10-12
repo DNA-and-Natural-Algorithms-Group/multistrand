0d5092f76189bcb5d999aa669f6c0527560b69ff
Joseph Schaeffer <schaeffer@dna.caltech.edu>
2010-10-12 08:20:21 -0700

    Fixed the annoying annoying issues in setup.py. No longer uses the C flag -Wstrict-prototyping, even if it thinks it really needs to [thus spitting out warnings because that doesn't apply to C++]. Also added some robustness for compilation with the tcmalloc library; should it not be found it just won't link against it.

--- setup.py
+++ setup.py
@@ -25,53 +25,102 @@ sources = ["interface/multistrand_module.cc",
 # setup.py for multistrand module.
 from distutils.core import setup, Extension
 
-import sys
-if '--use-debug-defs' in sys.argv:
-    sys.argv.remove('--use-debug-defs')
-    multi_ext = Extension("multistrand.system",
-                          sources=sources,
-                          include_dirs=["./include"],
-                          language="c++",
-                          define_macros=[('DEBUG',None),
-                                         ('DEBUG_MACROS',None),
-                                        ('Py_TRACE_REFS',None)],
-                          undef_macros=['NDEBUG'],
-                          #This is 'disable all warnings compiler flag' [possibly shouldn't be used for debug version]:
-                          extra_compile_args = ['-Wno-strict-prototypes','-w','-g','-O0'],
-                      )
-elif '--use-profiler-defs' in sys.argv:
-    sys.argv.remove('--use-profiler-defs')
-    multi_ext = Extension("multistrand.system",
-                          sources=sources,
-                          include_dirs=["./include"],
-                          language="c++",
-                          define_macros=[('DEBUG',None),
-                                         ('DEBUG_MACROS',None),
-                                        ('PROFILING',None)],
-                          undef_macros=['NDEBUG'],
-                          libraries=['profiler'],  #google perftools profiler.
-                          extra_compile_args = ['-w','-g','-O0'],
-                      )
+import distutils.sysconfig
+config_vars = distutils.sysconfig.get_config_vars()
+# config variables used in the next two functions.
+
+
+def setup_flags():
+    # remove -Wstrict-prototypes from our default flags for the compiler, if it's in there
+    # 
+    opt = config_vars['OPT'].split()
+    if '-Wstrict-prototypes' in opt:
+        opt.remove('-Wstrict-prototypes')
+        config_vars['OPT'] = ' '.join( opt )
+    cflags = config_vars['CFLAGS'].split()
+    if '-Wstrict-prototypes' in cflags:
+        cflags.remove('-Wstrict-prototypes')
+        config_vars['CFLAGS'] = ' '.join( cflags )
+    cppflags = config_vars['CPPFLAGS'].split()
+    if '-Wstrict-prototypes' in cppflags:
+        cppflags.remove('-Wstrict-prototypes')
+        config_vars['CPPFLAGS'] = ' '.join( cppflags )
+
+def setup_libcheck():
+    # Find the library path [so we can check for tcmalloc].
+    import os, os.path
+    ldflags = config_vars['LDFLAGS'].split()
+    for libpath in ldflags:
+        pathname = libpath[2:] # strip the -L from each entry
+        try:
+            items = iter( os.listdir(pathname) )
+        except OSError:
+            continue
+        for item in items:
+            if item.startswith('libtcmalloc'):
+                return True
+    return False
     
-else:
-    multi_ext = Extension("multistrand.system",
-                          sources=sources,
-                          include_dirs=["./include"],
-                          language="c++",
-                          libraries=['tcmalloc_minimal'],
-#                          define_macros=[('DEBUG',None),
-#                                         ('DEBUG_MACROS',None)],
-#                                         ('Py_TRACE_REFS',None)],
-                          undef_macros=['NDEBUG'],
-                          extra_compile_args = ['-O3','-g', '-w'],
-#                          ['-Wno-strict-prototypes','-w','-O0','-v','-fcommon', '-fno-wrapv'],   #This is 'disable all warnings compiler flag'
-                      )
 
-setup(name="multistrand", version="1.0",
-      packages=['multistrand','multistrand._options','multistrand._objects'],
-      package_dir={'multistrand':'interface'},
-      ext_modules=[multi_ext])
+def setup_ext( have_tcmalloc):    
+    import sys
+    if have_tcmalloc:
+        tcmalloc_optional = ['tcmalloc_minimal']
+    else:
+        tcmalloc_optional = []
+    if '--use-debug-defs' in sys.argv:
+        sys.argv.remove('--use-debug-defs')
+        multi_ext = Extension("multistrand.system",
+                              sources=sources,
+                              include_dirs=["./include"],
+                              language="c++",
+                              define_macros=[('DEBUG',None),
+                                             ('DEBUG_MACROS',None),
+                                             ('Py_TRACE_REFS',None)],
+                              undef_macros=['NDEBUG'],
+                              libraries=[] + tcmalloc_optional,
+                              #This is 'disable all warnings compiler flag' [possibly shouldn't be used for debug version]:
+                              extra_compile_args = ['-Wno-strict-prototypes','-w','-g','-O0'],
+                              )
+    elif '--use-profiler-defs' in sys.argv:
+        sys.argv.remove('--use-profiler-defs')
+        multi_ext = Extension("multistrand.system",
+                              sources=sources,
+                              include_dirs=["./include"],
+                              language="c++",
+                              define_macros=[('DEBUG',None),
+                                             ('DEBUG_MACROS',None),
+                                             ('PROFILING',None)],
+                              undef_macros=['NDEBUG'],
+                              libraries=['profiler'],  #google perftools profiler.
+                              extra_compile_args = ['-w','-g','-O0'],
+                              )
+        
+    else:
+        multi_ext = Extension("multistrand.system",
+                              sources=sources,
+                              include_dirs=["./include"],
+                              language="c++",
+                              libraries=[] + tcmalloc_optional,
+                              #                          define_macros=[('DEBUG',None),
+                              #                                         ('DEBUG_MACROS',None)],
+                              #                                         ('Py_TRACE_REFS',None)],
+                              undef_macros=['NDEBUG'],
+                              extra_compile_args = ['-O3','-g', '-w'],
+                              #                          ['-Wno-strict-prototypes','-w','-O0','-v','-fcommon', '-fno-wrapv'],   #This is 'disable all warnings compiler flag'
+                              )
+    return multi_ext
 
-#      packages=['interface','interface._options','interface._objects'],
-#      package_dir={'interface':''},
+if __name__ == '__main__':
+    setup_flags()
+    tcmalloc_flag = setup_libcheck()
+    multi_ext = setup_ext( have_tcmalloc=tcmalloc_flag)
+    
+    setup(name="multistrand", version="1.0",
+          packages=['multistrand','multistrand._options','multistrand._objects'],
+          package_dir={'multistrand':'interface'},
+          ext_modules=[multi_ext])
+
+    #      packages=['interface','interface._options','interface._objects'],
+    #      package_dir={'interface':''},
 
