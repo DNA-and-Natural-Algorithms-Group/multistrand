9ccabaec145794500d2baac439a8d588c4010aab
Joseph Berleant <jberleant@dna.caltech.edu>
2014-12-13 19:13:12 -0800

    Added/modified Python wrapper functions for Nupack executables pfunc, pairs, mfe, energy, prob, defect, sample. Added nupack_calls.py to the under_the_hood tutorials.

--- nupack/nupack_wrapper.py
+++ nupack/nupack_wrapper.py
@@ -1,43 +1,28 @@
 
-# Code contributors:  Erik Winfree, Chris Thachuk, Justin Bois.
+# Code contributors:  Erik Winfree, Chris Thachuk, Justin Bois, Joseph Berleant.
+#
+# The following functions are currently wrapped:
+#  pfunc
+#  pairs
+#  mfe
+#  energy
+#  prob
+#  defect
+#  sample
+#
+# The following functions may be wrapped in a future release:
+#  complexes
+#  count
+#  concentrations
+#  design
+#  distributions
+#  subopt
+
+
 
 import math
 import subprocess as sub
-
-# From Jason at Stackoverflow:
-class Memoize:
-    def __init__(self, f):
-        self.f = f
-        self.memo = {}
-    def __call__(self, *args):
-        if not args in self.memo:
-            self.memo[args] = self.f(*args)
-        return self.memo[args]
-# Note that the memoization here is useful when algorithms make repeated calls to evaluate the same sequences.
-# It comes at the expense of using an unbounded amount of memory.
-# A more sophisticated memoization would limit the number of memoized items, and forget the older ones.
-
-# from flebool on Stackoverflow
-def index_max(values):
-    """Gives the index of the maximum element."""
-    return max(xrange(len(values)),key=values.__getitem__)
-
-def index_min(values):
-    """Gives the index of the minimum element."""
-    return min(xrange(len(values)),key=values.__getitem__)
-
-def cyclic_sort(mylist):
-    """Rotates the list to put the "least" element first."""
-    i=index_min(mylist)
-    return mylist[i:]+mylist[:i]
-
-def rev(dom) : 
-    """Reverses a string."""
-    return dom[::-1]
-
-def WC(seq):
-    """Computes the Watson-Crick complement for a DNA sequence."""
-    return seq.translate(string.maketrans('ACTG','TGAC'))[::-1]
+import os
 
 def dGadjust(T,N):
     """Adjust NUPACK's native free energy (with reference to mole fraction units) to be appropriate for molar units, assuming N strands in the complex."""
@@ -47,86 +32,306 @@ def dGadjust(T,N):
     adjust = R*K*math.log(water) # converts from NUPACK mole fraction units to molar units, per association
     return adjust*(N-1)
 
+def get_nupack_exec_path(exec_name):
+  """ If the NUPACKHOME environment variable is set, use that as the directory
+  of the Nupack executables. Otherwise, have Python search the PATH directly. """
+  if 'NUPACKHOME' in os.environ:
+    return os.environ['NUPACKHOME'] + '/bin/' + exec_name;
+  else:
+    return exec_name;
+    
+def call_with_file(args, cmd_input, outsuffix):
+  """ Performs a NUPACK call, returning the lines of the output in a temporary
+  output file. The output file is assumed to have the suffix 'outsuffix'.
+  outsuffix includes the period (.) delimiter.
+    Ex:
+      call_with_file(args, input, '.sample')
+  """
+  
+  import tempfile
+
+  ## Preliminaries
+  # Set up temporary output file
+  outfile = tempfile.NamedTemporaryFile(delete=False, suffix=outsuffix)
+  outprefix = outfile.name[:-len(outsuffix)]
+  
+  # Close the output file so sample can open/write to it.
+  # Will reopen it later to get the output.
+  outfile.close()
+  
+  ## Perform executable call, ignoring pipe output
+  args = [str(s) for s in args] # all argument elements must be strings
+  cmd_input = outprefix + '\n' + cmd_input # prepend the output file prefix to the input for NUPACK
+  p = sub.Popen(args, stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.STDOUT)
+  p.communicate(cmd_input)
+  
+  ## Process and return output
+  # Read output file and clean it up
+  # Note that it was created by us, so it won't be cleaned up automatically
+  out = open(outfile.name, "rt")
+  output_lines = out.readlines()
+  out.close()
+  os.remove(outfile.name)
+  
+  return output_lines
+  
+
+def call_with_pipe(args, cmd_input):
+  """ Performs a NUPACK call, returning the lines of the output from the pipe.
+  """
+  args = [str(s) for s in args] # all argument elements must be strings
+  
+  p=sub.Popen(args,stdin=sub.PIPE,stdout=sub.PIPE,stderr=sub.PIPE)
+  output,error = p.communicate(cmd_input)
+  output_lines = output.split('\n')
+  return (output_lines, error)
+    
+def pfunc(sequences, material, dangles = 'some', T = 37, multi = True, pseudo = False, sodium = 1.0, magnesium = 0.0):
+  """Calls NUPACK's pfunc on a complex consisting of the unique strands in sequences, returns dG.
+       sequences is a list of the strand sequences
+       See NUPACK User Manual for information on other arguments. """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('pfunc'),
+          '-material', material,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences)
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order
+  
+  ## Perform call until it works (should we have a max # of tries before quitting?)
+  output, error = call_with_pipe(args, cmd_input)
+  while len(output) < 4 : # can't figure out why, but occasionally NUPACK returns empty-handed.  Subsequent tries seem to work...
+      print 'Retrying pfunc: NUPACK failed with output ' + `output` + ' and error ' + `error` +" ."
+      output, error = call_with_pipe(args, cmd_input)
+
+  ## Parse and return output
+  if output[-4] != "% Free energy (kcal/mol) and partition function:" :
+      raise NameError('NUPACK output parsing problem')
+
+  if float(output[-3])==float('inf') : return 0               # if these strands can't base-pair
+  else: return float(output[-3]) + dGadjust(T,len(sequences))
+  
+  
+def pairs(sequences, material, dangles = 'some',
+          T = 37, multi = True, pseudo = False,
+          sodium = 1.0, magnesium = 0.0, cutoff = 0.001):
+  """Calls NUPACK's pairs executable on a complex consisting of the unique strands in sequences.
+     Returns the probabilities of pairs of bases being bound, only including those pairs
+     with probability greater than cutoff.
+       sequences is a list of the strand sequences
+       See NUPACK User Manual for information on other arguments.
+  """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('pairs'),
+          '-material', material,   '-cutoff', cutoff,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences)
+    suffix = '.ppairs'
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order
+    suffix = '.epairs'
+  
+  ## Perform call
+  output = call_with_file(args, cmd_input, suffix)
+
+  ## Parse and return output
+  pair_probs = []
+  for l in filter(lambda x: x[0].isdigit(), output):
+    if len(l.split()) > 1:
+      pair_probs.append(tuple(l.split()))
+  
+  return pair_probs
+
+  
+def mfe(sequences, material, dangles = 'some',
+        T = 37, multi = True, pseudo = False,
+        sodium = 1.0, magnesium = 0.0, degenerate = False):
+  """Calls NUPACK's mfe executable on a complex consisting of the unique strands in sequences.
+     Returns the minimum free energy structure, or multiple mfe structures if the degenerate
+     option is specified
+       sequences is a list of the strand sequences
+       degenerate is a boolean specifying whether to include degenerate mfe structures
+       See NUPACK User Manual for information on other arguments.
+  """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('mfe'),
+          '-material', material,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  if degenerate: args += ['-degenerate']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences)
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order
+  
+  ## Perform call
+  output = call_with_file(args, cmd_input, '.mfe')
+
+  ## Parse and return output
+  structs = []
+  for i, l in enumerate(output):
+    if l[0] == '.' or l[0] == '(':
+      s = l.strip()
+      e = output[i-1].strip()
+      structs.append((s,e))
+  
+  return structs
+  
+  
+def energy(sequences, structure, material, dangles = 'some', T = 37, multi = True, pseudo = False, sodium = 1.0, magnesium = 0.0):
+  """Calls NUPACK's energy executable. Returns the microstate dG.
+       sequences is a list of the strand sequences
+       structure is a string with the dot-paren structure notation
+         (pair-list notation for structures is not currently supported)
+       See NUPACK User Manual for information on the other arguments.
+  """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('energy'),
+          '-material', material,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences) + '\n' + structure
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order + '\n' + structure
+                
+  ## Perform call
+  output, error = call_with_pipe(args, cmd_input)
+
+  ## Parse and return output
+  if output[-3] != "% Energy (kcal/mol):" :
+     raise ValueError('NUPACK output parsing problem')
+
+  return float(output[-2])
 
 
-def energy(seq,structure,material,T):
-    """Calls NUPACK's 'energy' for a given sequence and secondary structure pair.  Returns the microstate dG.  T is in Celsius."""
-    nupack_input = str(1) + '\n' + seq + '\n' + str(1) + '\n' + structure + '\n'
-    p = sub.Popen(['energy', '-T', str(T), '-multi', '-material', material], stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
-    output = p.communicate(nupack_input)[0]
-    lines = output.split('\n')
+def prob(sequences, structure, material, dangles = 'some', T = 37, multi = True, pseudo = False, sodium = 1.0, magnesium = 0.0):
+  """Calls NUPACK's prob executable. Returns the probability of the given structure.
+       sequences is a list of the strand sequences
+       structure is a string with the dot-paren structure notation
+         (pair-list notation for structures is not currently supported)
+       See NUPACK User Manual for information on the other arguments.
+  """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('prob'),
+          '-material', material,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences) + '\n' + structure
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order + '\n' + structure
+                
+  ## Perform call
+  output, error = call_with_pipe(args, cmd_input)
 
-    if lines[-3] != "% Energy (kcal/mol):" :
-       raise ValueError('NUPACK output parsing problem')
+  ## Parse and return output
+  if output[-3] != "% Probability:" :
+     raise ValueError('NUPACK output parsing problem')
 
-    return float(lines[-2])
+  return float(output[-2])
 
 
-@Memoize
-def pfunc_tuple(seqtuple,T):
-    """Calls NUPACK's pfunc on a complex consisting of the unique strands in seqtuple, returns dG.  T is in Celsius."""
-    user_input = str(len(seqtuple)) + '\n' + '\n'.join(seqtuple) + '\n' + str(range(1,len(seqtuple)+1))[1:-1].replace(',','')
-    p=sub.Popen(['pfunc','-T',str(T),'-multi','-material','dna'],stdin=sub.PIPE,stdout=sub.PIPE,stderr=sub.PIPE)
-    output,error = p.communicate(user_input)
-    lines = output.split('\n')
+def defect(sequences, structure, material,
+           dangles = 'some', T = 37, multi = True, pseudo = False,
+           sodium = 1.0, magnesium = 0.0, mfe = False):
+  """Calls NUPACK's defect executable. Returns the ensemble defect (default) or the mfe defect.
+       sequences is a list of the strand sequences
+       structure is a string with the dot-paren structure notation
+         (pair-list notation for structures is not currently supported)
+       See NUPACK User Manual for information on the other arguments.
+  """
+  
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('defect'),
+          '-material', material,   '-sodium', sodium,
+          '-magnesium', magnesium, '-dangles', dangles, '-T', T]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  if mfe: args += ['-mfe']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences) + '\n' + structure
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs) + '\n' + ('\n'.join(sequences)) + '\n' + seq_order + '\n' + structure
+                
+  ## Perform call
+  output, error = call_with_pipe(args, cmd_input)
 
-    while len(lines) < 4 : # can't figure out why, but occasionally NUPACK returns empty-handed.  Subsequent tries seem to work...
-        print 'Retrying in pfunc_tuple: NUPACK failed with output ' + `lines` + ' and error ' + `error` +" ."
-        p=sub.Popen(['pfunc','-T',str(T),'-multi','-material','dna'],stdin=sub.PIPE,stdout=sub.PIPE,stderr=sub.PIPE)
-        output,error = p.communicate(user_input)
-        lines = output.split('\n')
+  ## Parse and return output
+  if "% Ensemble defect" not in output[-4] and \
+        "% Fraction of correct nucleotides vs. MFE" not in output[-4]:
+    raise ValueError('NUPACK output parsing problem')
 
-    if lines[-4] != "% Free energy (kcal/mol) and partition function:" :
-        raise NameError('NUPACK output parsing problem')
+  # We don't return the normalized ensemble defect, because that is easily calculable on your own
+  return float(output[-3])
 
-    if float(lines[-3])==float('inf') : return 0               # if these strands can't base-pair
-    else: return float(lines[-3]) + dGadjust(T,len(seqtuple))
 
-# We have pfunc_tuple because tuples but not lists can be memoized.  But lists are clearer notationally.
-# An alternative syntax to the "@Memoize" line would be, here: pfunc_tuple=Memoize(pfunc_tuple)
-# Note that since pfunc give identical results for any rotation of strand order, we use a canonical order that aids memoization.
+def sample(samples, sequences, material,
+           dangles = 'some', T = 37, multi = True,
+           pseudo = False, sodium = 1.0, magnesium = 0.0):
+  """ Calls the NUPACK sample executable.
+        samples is the number of Boltzmann samples to produce.
+        See NUPACK User Manual for information on the other arguments.
+      This only works with NUPACK 3.0.2+
+      
+      Note that if using OS X and sample is not in your $PATH, this will try
+      to run the standard BSD tool 'sample'. """
+      
+  ## Set up command-line arguments and input
+  args = [get_nupack_exec_path('sample'),
+          '-material', material, '-sodium', sodium, '-magnesium', magnesium,
+          '-dangles', dangles, '-T', T, '-samples', samples]
+  if multi: args += ['-multi']
+  if pseudo: args += ['-pseudo']
+  
+  if not multi:
+    cmd_input = '+'.join(sequences);
+  else:
+    n_seqs = len(sequences)
+    seq_order = ' '.join([str(i) for i in range(1, n_seqs+1)])
+    cmd_input = str(n_seqs)          + '\n' + \
+                '\n'.join(sequences) + '\n' + \
+                seq_order
 
-def pfunc(seqlist,T) : return pfunc_tuple(tuple(cyclic_sort(seqlist)),T)
+  # Call executable
+  output = call_with_file(args, cmd_input, '.sample')
 
-@Memoize
-def duplex_energy(seq,T):
-    """Calls NUPACK's 'energy' on a complex consisting of two complementary strands, returns the microstate dG.  T is in Celsius."""
-    seqtuple=(seq,WC(seq))
-    user_input = str(len(seqtuple)) + '\n' + '\n'.join(seqtuple) + '\n' + str(range(1,len(seqtuple)+1))[1:-1].replace(',','') + '\n' + '('*len(seq)+'+'+')'*len(seq)
-    p=sub.Popen(['energy','-T',str(T),'-multi','-material','dna'],stdin=sub.PIPE,stdout=sub.PIPE,stderr=sub.PIPE)
-    output = p.communicate(user_input)[0]
-    lines = output.split('\n')
+  # Check NUPACK version
+  if not "NUPACK 3.0" in output[0]:
+    raise IOError("Boltzmann sample function is not up to date. NUPACK 3.0.2 or greater needed.")
 
-    while len(lines) < 3 : # can't figure out why, but occasionally NUPACK returns empty-handed.  Subsequent tries seem to work...
-        print 'Retrying in duplex_energy: NUPACK failed with output ' + `lines` + ' and error ' + `error` +" ."
-        p=sub.Popen(['energy','-T',str(T),'-multi','-material','dna'],stdin=sub.PIPE,stdout=sub.PIPE,stderr=sub.PIPE)
-        output,error = p.communicate(user_input)
-        lines = output.split('\n')
-
-    if lines[-3] != "% Energy (kcal/mol):" :
-       raise ValueError('NUPACK output parsing problem')
-
-    return float(lines[-2]) + dGadjust(T,len(seqtuple))
-
-# PyUtils has:
-# getEnergy    energy
-# getProb      prob
-# getMFEStruct mfe
-# getPairProbs pairs
-
-# we want:
-# sample
-# defect
-# energy --- need to allow structures other than duplex // multistranded things.  add salt.
-# pfunc  --- need to make sure it works for any number of complexes.  add material and salt.
-# prob
-# mfe
-# pairs
-
-# other nupack functions that aren't as high priority right now, are:
-# complexes
-# count
-# concentrations
-# design
-# distributions
-# subopt
+  # Parse and return output
+  sampled = [l.strip() for l in output[14:]]
+  return sampled
---
+++ tutorials/under_the_hood/nupack_calls.py
@@ -0,0 +1,100 @@
+# nupack_calls.py
+#
+# Multistrand is equipped with Python wrapper functions for many of Nupack's executables, including
+# pfunc, energy, defect, prob, sample, mfe, and pairs. Here we provide usage examples for all
+# provided wrapper functions. See the Nupack User Manual for more information about the arguments to
+# each executable. Many of these examples are adapted from examples provided with Nupack.
+# Note that these functions assume you have Nupack version 3.0.2 or higher. Certain functions, such
+# as sample(), will not work with earlier versions of Nupack.
+#
+# In general, the following arguments are required by all functions:
+#   sequences  - a list of DNA/RNA sequences
+#   structure  - a dot-paren structure (required only by energy, prob, and defect)
+#   material   - a string representing the material parameters to use (e.g. 'rna' or 'dna')
+#               The Nupack User Manual has a complete list of possible values.
+# The following optional arguments may also be included in all function calls:
+#   multi      - whether or not to allow multistrand structures with identical strands
+#                only works with DNA (default: True)
+#   pseudo     - whether or not to allow pseudoknotted structures
+#                only works with RNA (default: False)
+#   dangles    - 'none', 'all', or 'some' to specify what types of dangle corrections to use (default: 'some')
+#   T          - temperature of the system in degrees C (default: 37)
+#   sodium     - concentration of Na+ in molar (default: 1.0)
+#   magnesium  - concentration of Mg++ in molar (default: 0.0)
+#
+# The following optional arguments are allowed by only some functions:
+#   cutoff     - (for use with pairs) probabilities less than cutoff are excluded (default: 0.001)
+#   degenerate - (for use with mfe) degenerate structures are all returned (default: False)
+#   mfe        - (for use with defect) returns mfe defect rather than ensemble defect (default: False)
+
+
+## Sequences used throughout this file:
+rna_seq = ['GGGCUGUUUUUCUCGCUGACUUUCAGCCCCAAACAAAAAAUGUCAGCA'];
+dna_seqs = ['AGTCTAGGATTCGGCGTGGGTTAA',
+            'TTAACCCACGCCGAATCCTAGACTCAAAGTAGTCTAGGATTCGGCGTG',
+            'AGTCTAGGATTCGGCGTGGGTTAACACGCCGAATCCTAGACTACTTTG']
+dna_struct = '((((((((((((((((((((((((+))))))))))))))))))))))))((((((((((((.(((((((((((+........................))))))))))).))))))))))))'
+
+
+## Examples with pfunc()
+# Find the partition function of this set of three DNA strands.
+pfunc(dna_seqs, 'dna')
+# Output: -62.66453454292297
+
+# Find the partition function of this single RNA strand, including pseudoknotted structures.
+pfunc(rna_seq, 'rna', multi = False, pseudo = True)
+# Output: -17.2786076
+
+
+## Example with energy()
+# Find the energy (in kcal/mol) of this DNA structure.
+energy(dna_seqs, dna_struct, 'dna')
+# Output: -61.79270283815892
+
+## Example with defect()
+# Find the ensemble defect of this DNA structure.
+defect(dna_seqs, dna_struct, 'dna')
+# Output: 8.297
+
+# Find the mfe defect of this DNA structure.
+defect(dna_seqs, dna_struct, 'dna', mfe = True)
+# Output: 2.0
+
+## Examples with prob()
+# Find the probability of this DNA structure.
+prob(dna_seqs, dna_struct, 'dna')
+# Output: 7.992e-05
+
+## Examples with sample()
+# Boltzmann sample 3 structures with these DNA strands.
+sample(3, dna_seqs, 'dna')
+# Example output:
+#  ['.(((((((((((((((((((((((+))))))))))))))))))))))).((((((((((((((((((((((((+..............(...).....))))))))))))))))))))))))',
+#   '(((((((((((((((((((((((.+.))))))))))))))))))))))).(((((.(((((((((((((((((+........................))))))))))))))))).))))).',
+#   '((((((((((((((((((((((..+..))))))))))))))))))))))..((((((((((((((((((((((+........................))))))))))))))))))))))..']
+
+## Examples with mfe()
+# Find the MFE structure of this ordering of DNA sequences (the energy is also provided).
+mfe(dna_seqs, 'dna')
+# Output:
+#  [('((((((((((((((((((((((((+))))))))))))))))))))))))((((((((((((((((((((((((+........................))))))))))))))))))))))))',
+#   '-65.913')]
+
+## Examples with pairs()
+# Find the pair probabilities of the following unpseudoknotted DNA strands.
+# The output is a list of tuples of the form (i, j, p) where p is the
+# probability that the ith and jth bases are bound.
+pairs(['ACGT','GCTT'], 'dna')
+# Output:
+#  [('1', '7', '3.1271e-02'),
+#   ('1', '8', '4.9495e-02'),
+#   ('2', '5', '2.1630e-01'),
+#   ('3', '6', '7.3677e-01'),
+#   ('1', '9', '9.1923e-01'),
+#   ('2', '9', '7.8370e-01'),
+#   ('3', '9', '2.6323e-01'),
+#   ('4', '9', '1.0000e+00'),
+#   ('5', '9', '7.8370e-01'),
+#   ('6', '9', '2.6323e-01'),
+#   ('7', '9', '9.6873e-01'),
+#   ('8', '9', '9.5051e-01')]
