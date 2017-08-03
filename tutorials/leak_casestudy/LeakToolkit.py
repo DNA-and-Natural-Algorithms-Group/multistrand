107dfbe7d7a12ea2b76f5992b8683515b84bb206
MrinankSharma <mrinank.sharma.97+dev@gmail.com>
2017-08-03 15:15:36 -0700

    Revert "Merge remote-tracking branch 'upstream/fdann-devel' into ms_devel"
    This reverts commit 984db8a6eb2f736f707ad4849c6ecb13cbd466cf, reversing
    changes made to 8a2489dea5da4bae453f571103b190ad54f4d2bc.

--- tutorials/leak_casestudy/LeakToolkit.py
+++ tutorials/leak_casestudy/LeakToolkit.py
@@ -19,7 +19,7 @@ for x in dirs:
 from multistrand.concurrent import myMultistrand, MergeSim, FirstStepRate, Bootstrap
 from multistrand.objects import StopCondition
 from multistrand.options import Options
-from msArrhenius import setArrheniusConstantsDNA23
+from multistrandPy.msArrhenius import setArrheniusConstantsDNA23
 
 from SeesawGate import SeesawRates
 import numpy as np
@@ -33,7 +33,7 @@ INCREMENT_TRIALS = 2000
 DNA = "DNA"
 
 
-myMultistrand.setNumOfThreads(4)
+myMultistrand.setNumOfThreads(2)
 myMultistrand.setTerminationCriteria(MINIMUM_FORWARD)
 myMultistrand.setLeakMode()
 
